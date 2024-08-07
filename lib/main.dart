b8393f62eeebcd4fa7d49a1ff11d2df0e78023c5
peteraziz <peet.a.91@gmail.com>
2024-08-07 15:21:42 +0300

    add firebase project todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nebu_guid_master/providers/FirestoreDataBase.dart';
 import 'package:nebu_guid_master/providers/GuideProvider.dart';
 import 'package:nebu_guid_master/screens/Info_Bank_Screen.dart';
 import 'package:nebu_guid_master/screens/Restrictions_Screen.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -15,6 +16,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  /// todo add firebase project config and init.
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
