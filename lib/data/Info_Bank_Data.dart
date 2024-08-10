c9d81ab74af743e31949d09dd9bcb5ff11b3aeef
beshoy <beshoynebu97@gmail.com>
2024-08-10 17:11:14 +0300

    add 11 info records to the bank

--- lib/data/Info_Bank_Data.dart
+++ lib/data/Info_Bank_Data.dart
@@ -7,9 +7,9 @@ InfoBank SalesMargin = InfoBank(
   Tip_Section: "مبيعات" ,
   Related_Screen:"شاشة المبيعات" ,
   Tip_Order_Number: '1',
+  Tip_Main_Description:  "  يتم تغيير قيمة المصنعية عن طريق تغيير مكان المؤشر ، تحريكه يمينا تقل المصنعية ،اما يسارا فتزيد المصنعية مع تحديث السعر الاجمالي في نفس الوقت ",
   Tip_Description_Idea:   " الهدف من تصميم مؤشز تغيير المصنعية هو التسهيل علي المستخدم و ايضا منع الاخطاء الحسابية و منع الحسابات المكررة ",
   Tip_Description_Info:   " هل تعلم ان متوسط وقت حساب اجمالي قيمة منتج  بالطريقة اليدوية هي ٤٥ ثانية اما عن طريق النظام  فهي ٥ ثوان فقط",
-  Tip_Main_Description:  "  يتم تغيير قيمة المصنعية عن طريق تغيير مكان المؤشر ، تحريكه يمينا تقل المصنعية ،اما يسارا فتزيد المصنعية مع تحديث السعر الاجمالي في نفس الوقت ",
   Tip_Tech_Details: " يمكن تغيير المصنعية حتي و ان اختفت " ,
 
 
@@ -44,6 +44,425 @@ InfoBank SalesMargin = InfoBank(
   Is_ForOwner: true,
 
 );
+InfoBank SalesTotalPerItem = InfoBank(
+  Tip_Title: "تغيير اجمالي(جنيه)",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '2',
+  Tip_Main_Description:  " يتم تعديل اجمالي(جنيه) عن طريق الضغط علي زر اجمالي(جنيه) ثم ادخال قيمة بين اقل قيمة واقصي قيمة  ",
+  Tip_Description_Idea:   " الهدف من تغيير الاجمالي بالجنيه هو التسهيل علي المستخدم حساب المصنعية لمنع الاخطاء وتوفير الوقت ",
+  Tip_Description_Info:   " هل تعلم ان متوسط وقت حساب اجمالي قيمة منتج  بالطريقة اليدوية هي ٤٥ ثانية اما عن طريق النظام  فهي ٥ ثوان فقط",
+  Tip_Tech_Details: " " ,
+
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank ItemPriceDetails = InfoBank(
+  Tip_Title: "عرض تفاصيل السعر للبند",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '3',
+  Tip_Main_Description:  " يتم عرض تفاصيل السعر للبند عن طريق الضغط علي تفاصيل السعر   ",
+  Tip_Description_Idea:   " الهدف هو عرض اجمالي قيمة الذهب واجمالي قيمة المصنعية واجمالي قيمة الضرايب علي حدا للتوضيح  ",
+  Tip_Description_Info:   "الهدف هو عرض اجمالي قيمة الذهب واجمالي قيمة المصنعية واجمالي قيمة الضرايب علي حدا للتوضيح",
+  Tip_Tech_Details: "  " ,
+
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank ItemSizeChange = InfoBank(
+  Tip_Title: "تغيير مقاس البند",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '4',
+  Tip_Main_Description:  " يتم تغيير مقاس البند عن طريق الضغط علي تغيير مقاس ثم تفعيل تفعيل الخاصية وإدخال الوزن الصافي بعد التوسيع او التضييق علي الميزان   ",
+  Tip_Description_Idea:   " الهدف هو سهولة توسيع او تضييق اي قطعة ، فاذا كان توسيع فسيخرج وزن الكسر من رصيد الكسر وسيدخل في رصيد البضاعة بشكل لحظي بعد اتمام العملية والعكس صحيح  ",
+  Tip_Description_Info:   "زيادة مقاس: سيتم خروج الزيادة من الكسر مباشرة واضافتها لرصيد البضاعة، تصغير مقاس:سيتم دخول الفرق للكسر،وخروج قيمته من بند البضاعة",
+  Tip_Tech_Details: "  " ,
+
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank ItemDeletion = InfoBank(
+  Tip_Title: "مسح بند من فاتورة",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '5',
+  Tip_Main_Description:  " يتم مسح اي بند من الفاتورة عن طريق الضغط علي ايقونة الفاتورة اعلي يسار الصفحة ستنقلنا الي صفحة تفاصيل الفاتورة ،ثم نختار البند الذي نريد حذفه بالضغط عليه ثم الضغط علي علامة السلة   ",
+  Tip_Description_Idea:   " الهدف هو التسهيل علي المستخدم مسح اي قطعة اذا اراد العميل عدم شرائها او تبديلها بقطعة اخري  ",
+  Tip_Description_Info:   "هل تعلم ان وقت مسح اي بند من الفاتورة لا يستغرق 3 ثواني علي عكس الفواتير الورقية سيتطلب منك الغاء الفاتورة واعادة كتابتها مرة اخري",
+  Tip_Tech_Details: "  " ,
+
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank ItemEditData = InfoBank(
+  Tip_Title: "مسح بند من فاتورة",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '6',
+  Tip_Main_Description:  " يتم مسح اي بند من الفاتورة عن طريق الضغط علي ايقونة الفاتورة اعلي يسار الصفحة ستنقلنا الي صفحة تفاصيل الفاتورة ،ثم نختار البند الذي نريد تعديله ثم الضغط علي ايقونة القلم باللون الاخضر ثم تعديل وتعديل البيانات المطلونة ثم الضغط علي اضافة مبيعات بعد التعديل   ",
+  Tip_Description_Idea:   " الهدف هو التسهيل علي المستخدم تعديل اي قطعة اذا اختار العميل قطعة اخري  ",
+  Tip_Description_Info:   "هل تعلم ان وقت مسح اي بند من الفاتورة لا يستغرق 3 ثواني علي عكس الفواتير الورقية سيتطلب منك الشطب للتعديل او الغاء الفاتورة واعادة كتابتها مرة اخري",
+  Tip_Tech_Details: "  " ,
+
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank ItemDeleteData = InfoBank(
+  Tip_Title: "مسح بيانات المبيعات",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '7',
+  Tip_Main_Description:  " يتم مسح اي بيانات تم ادخالها في صفحة تفاصيل المبيعات عن طريق الضغط علي علامة السلة ",
+  Tip_Description_Idea:   " الهدف هو التسهيل علي المستخدم مسح اي بيانات خاطئة  ",
+  Tip_Description_Info:   "هل تعلم انه يمكنك مسح جميع بيانات المبيعات التي تم ادخالها بضغطة زر بدلا من مسحها واحدة تلو الاخري",
+  Tip_Tech_Details: "  " ,
+
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank GoldbarSerialScan = InfoBank(
+  Tip_Title: "scan سيريال السببيكة",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '8',
+  Tip_Main_Description:  " يتم عمل (Scan) لسيريال السبيكة عن طريق الضغط علي ايقونة الفاتورة اعلي يسار الصفحة ، ثم اختيار بند السبيكة ثم الضغط علي الايقونة الزرقاء لمسح السيريال    ",
+  Tip_Description_Idea:   " الهدف هو توثيق كود السبيكة علي السيستم وكتابتها علي فاتورة العميل لتفادي الغش  ",
+  Tip_Description_Info:   "هل تعلم ان تسجيل سيريال السبيكة علي السيستم سيحميك لاحقا من اي غش محتمل من قبل اي عميل ",
+  Tip_Tech_Details: "  " ,
+
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank PrintInvoice = InfoBank(
+  Tip_Title: "طباعة فاتورة",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '9',
+  Tip_Main_Description:  " يتم طباعة الفاتورة عن طريق الضغط علي أيقونة الطباعة اعلي يسار الصفحة ، ثم أختيار الطابعة المتصلة بالشبكة ثم اختيار مقاس الورقة (A5) ثم الضغط علي Print لطباعة الفاتورة    ",
+  Tip_Description_Idea:   " الهدف هو اعطاء كل عميل فاتورة خاصة به يسهل عليه قرائتها ولا يوجد بها اي اخطاء املائية أو حسابية ",
+  Tip_Description_Info:   "هل تعلم ان طباعة اي فاتورة لا يستغرق 3 ثواني ولا يوجد بها اخطاء  ",
+  Tip_Tech_Details: "  " ,
+
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank InvoicesArchive = InfoBank(
+  Tip_Title: "أرشيف الفواتير ",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '10',
+  Tip_Main_Description:  " يتم الذهاب إلي ارشيف الفواتير عن طريق الضغط علي الايقونة الزرقاء اعلي يسار الصفحة ثم الضغط علي ارشيف الفواتير ثم الذهاب لاي فاتورة بالأرشيف ",
+  Tip_Description_Idea:   " الهدف هو التسهيل علي المستخدم حفظ اي فاتورة اذا اراد العميل مغادرة المحل والعودة مرة اخري لاي سبب ",
+  Tip_Description_Info:   "هل تعلم ان ارشيف الفواتير يوفر الوقت والجهد للمستخدم ويسهل عليه العودة لاي فاتورة ",
+  Tip_Tech_Details: "  " ,
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+InfoBank AddItemToInvoice = InfoBank(
+  Tip_Title: "إضافة بند للفاتورة ",
+  Tip_Section: "مبيعات" ,
+  Related_Screen:"شاشة المبيعات" ,
+  Tip_Order_Number: '11',
+  Tip_Main_Description:  " يتم إضافة اي بند لفاتورة المبيعات عن طريق الضغط علي اضافة مبيعات ثم الضغط علي تأكيد  ",
+  Tip_Description_Idea:   " الهدف هو التسهيل علي المستخدم اضافة اي بند بسهولة للفاتورة ",
+  Tip_Description_Info:   "هل تعلم ان إضافة اي بند للفاتورة يستغرق وقتا قليلا جدا ",
+  Tip_Tech_Details: "  " ,
+
+  // main info type
+  Is_Basic: true ,
+  Is_New: false ,
+  Is_FAQ: false ,
+  Is_Official: false ,
+  Is_Step_By_Step: false ,
+
+  // material type , lottie => animation
+  Is_Material_Lottie: true  ,
+  Is_Material_Picture: false ,
+  Is_Material_YouTube: false ,
+
+  // todo development part
+  Related_App_Screen: "Sales_Screen",
+  Material_Path: "https://firebasestorage.googleapis.com/v0/b/nebu-6ff28.appspot.com/o/tips%2Fmargin_slider_compressed.json?alt=media&token=9b35877b-1d5b-4eb6-84aa-a7135b81b72a" ,
+  Material_Path_List: [] ,
+  Android_Ver:133 ,
+  IOS_Ver: 94,
+
+  // Fixed values do not change
+  Is_Optional: false ,
+  Is_Required: true,
+  Is_Take_Time: false,
+  Is_ComingSoon: false ,
+
+  // roles
+  Is_For_Admin: true,
+  Is_For_SalesTeam: true,
+  Is_ForOwner: true,
+
+);
+
 
 /// Purchase
 /// Goods
