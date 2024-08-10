bc98dfc4c978b55ef0dbd61557cbccb3f8800b2e
beshoy <beshoynebu97@gmail.com>
2024-08-10 14:36:54 +0300

    add first info

--- lib/data/Restrictions_Data.dart
+++ lib/data/Restrictions_Data.dart
@@ -27,14 +27,21 @@ Restriction R1 =  Restriction(
   Allowed_Plans:  ["basic", "pro", "vip"] ,
   Short_Description: "Navigation To System Migration " ,
   Blurred: true ,
-  Description:  ,
-  Description_Ar: ,
-  Dev_Mode: ,
-  Material_Type: ,
-  Photo_Url:  ,
-  Role_Permission: ,
-  Short_Description_Ar: ,
-  Store_Type:  ,
-  Visible:
+  Description: "To go to the migration from an old system to save the codes" ,
+  Description_Ar: " الذهاب الي تسجيل النظام السابق ",
+  Dev_Mode: false,
+  Material_Type:  ["Gold_Silver", "Gold", "Silver"],
+  Photo_Url: "" ,
+  Role_Permission: [
+    "Owner",
+    "Manager",
+    "Purchaser",
+    "Accountant",
+  ],
+  Short_Description_Ar:  "لتسجيل الاكواد بالنظام السابق الي النظام الجديد"  ,
+  Store_Type: [
+    "Retail",
+  ] ,
+  Visible:true,
 );
 
